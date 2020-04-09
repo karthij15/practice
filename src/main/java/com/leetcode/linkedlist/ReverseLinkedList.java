8f75154c2bf8bff3288510a4083738ef80c7628b
Karthi Janaki Raman <karthij.upgrade@gmail.com>
2020-04-09 08:40:44 +0530

    Create ReverseLinkedList.java

---
+++ src/main/java/com/leetcode/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,72 @@
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+*/
+
+//Based On: https://medium.com/@jimdaosui/reverse-linked-list-ca8be5651326
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+ }
+ 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseListUsingRecursion(head);
+    }
+    
+   public ListNode reverseListUsingRecursion(ListNode head) {
+       //1->2->3->4->5->NULL
+        if(head == null || head.next == null)
+            return head;
+        
+       ListNode lastNode = reverseListUsingRecursion(head.next);
+       head.next.next = head;
+       head.next = null;       
+       
+       return lastNode;
+    }
+    
+    
+    public ListNode reverseListUsingIteration(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = head;
+        
+        while(current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+    
+    public ListNode reverseListUsingStack(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        
+        while(head != null) {
+            stack.push(new ListNode(head.val));
+            head = head.next;
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        head = dummy.next;
+        
+        while(!stack.isEmpty()) {
+            head = stack.pop();
+            head = head.next;
+        }
+        
+        return dummy.next;
+    }
+}
