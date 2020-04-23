48b88f7962d4a3eed1fe50b821d758f3007c5ce3
Karthi Janaki Raman <karthij.upgrade@gmail.com>
2020-04-23 12:50:50 +0530

    Create ValidParenthesisString.java

---
+++ src/main/java/com/leetcode/thirtydaychallenge/ValidParenthesisString.java
@@ -0,0 +1,61 @@
+package com.leetcode.thirtdaychallenge;
+
+import java.util.Stack;
+
+/*
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+An empty string is also valid.
+Example 1:
+Input: "()"
+Output: True
+Example 2:
+Input: "(*)"
+Output: True
+Example 3:
+Input: "(*))"
+Output: True
+Note:
+The string size will be in the range [1, 100].
+*/
+
+//store indexes in stack
+
+class ValidParenthesisString {
+    public boolean solution(String s) {
+        Stack<Integer> openBracesStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
+
+        int i = 0;
+
+        while(i < s.length()) {
+
+            if(s.charAt(i) == '(') 
+                openBracesStack.push(i);
+            else if(s.charAt(i) == '*') 
+                starStack.push(i);
+            else { //for ')'
+
+                if(!openBracesStack.isEmpty())
+                    openBracesStack.pop();
+                else if(!starStack.isEmpty()) 
+                    starStack.pop();
+                else 
+                    return false;
+            }
+            
+            i++;
+        }
+        
+        while(!openBracesStack.isEmpty()) {
+            if(starStack.isEmpty() || openBracesStack.pop() > starStack.pop())
+                return false;
+        }
+       
+        return openBracesStack.isEmpty();
+    }
+}
