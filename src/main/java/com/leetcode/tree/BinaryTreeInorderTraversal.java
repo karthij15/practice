2491e8c22e8aad1cf93b1f0d4281e5d6c8c41479
Karthi Janaki Raman <karthij.upgrade@gmail.com>
2020-04-28 00:14:06 +0530

    Create BinaryTreeInorderTraversal.java

---
+++ src/main/java/com/leetcode/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,45 @@
+package com.leetcode.tree;
+
+/*
+In-order traversal is to traverse the left subtree first. Then visit the root. Finally, traverse the right subtree.
+
+Let's do in-order traversal together:
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [1,3,2]
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+class BinaryTreeInorderTraversal {
+    public List<Integer> solution(TreeNode root) {
+        
+        List<Integer> values = new ArrayList<>();
+        TreeNode node = root;
+        Stack<TreeNode> nodeStack = new Stack<>();
+        
+        while(node != null || !nodeStack.isEmpty()) {//only check to traverse
+           
+            if(node != null) {
+                nodeStack.push(node);
+                node = node.left;
+            } else {
+                node = nodeStack.pop();
+                values.add(node.val);
+                node = node.right;
+            }
+        }
+        
+        return values;        
+        
+    }
+}
