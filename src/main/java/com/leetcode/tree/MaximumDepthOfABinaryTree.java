9a47c1498e3fe120ae62df246542ec8c3de14527
Karthi Janaki Raman <karthij.upgrade@gmail.com>
2020-04-28 13:05:57 +0530

    Update MaximumDepthOfABinaryTree.java

--- src/main/java/com/leetcode/tree/MaximumDepthOfABinaryTree.java
+++ src/main/java/com/leetcode/tree/MaximumDepthOfABinaryTree.java
@@ -49,3 +49,59 @@ Or, you can think of the problem in this way: for a node in a tree, if you know 
 
 In the following sections, we provide several classic problems for you to help you understand tree structure and recursion better.
 */
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MaximumDepthOfABinaryTree {
+    int answer;
+    
+    public int maxDepth(TreeNode root) {
+    //Bottom-up - similar to Post Order
+        if(root == null)
+            return 0;
+        
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+      
+      //Top-Down Similar to Pre Order
+      /*
+      maximum_depth(root, 1);
+      return answer;
+      */
+    }
+  
+    private void maximum_depth(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            answer = Math.max(answer, depth);
+        }
+        maximum_depth(root.left, depth + 1);
+        maximum_depth(root.right, depth + 1);
+    }
+}
