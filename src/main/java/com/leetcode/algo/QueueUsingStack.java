7cee51a174ce59d71a8588c07ddbb3b961af1e0c
Karthi Janaki Raman <karthij.upgrade@gmail.com>
2020-10-30 14:38:44 +0530

    Create QueueUsingStack.java

---
+++ src/main/java/com/leetcode/algo/QueueUsingStack.java
@@ -0,0 +1,31 @@
+package com.leetcode.algo;
+
+/*
+Implementing Queue using Stack
+*/
+
+class QueueUsingStack {
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public void pop() {
+        peek();
+        output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
